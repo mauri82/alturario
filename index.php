1f4563e5c880f885089995d8650dc3ac77108982
mauri82 <mauriciogz@gmail.com>
2018-06-21 18:58:08 -0300

    Casi listo

--- index.php
+++ index.php
@@ -1,7 +1,46 @@
 <?php 
+// Inicio Consulta altura Rosario 
+$url='http://sitiowcontingencia.prefecturanaval.gov.ar/alturas/index.php';
+$html = file_get_contents($url);
+//$inicio=strpos($html,'<table class="table table-hover fpTable">');
+//$fin=strpos($html,'<table class="table table-hover fpTable">');
+$inicio=strpos($html,'<th scope="row">280</th>');
+$fin=strpos($html,'<th scope="row">290</th>');
+$largo=$fin-$inicio;
+$htmlRosario=substr($html,$inicio,$largo);
+$html='';
+$htmlRosario=str_replace('<th scope="row">280</th> <th scope="row" class="pna-port-left">ROSARIO</th> <td>PARANA</td>','',$htmlRosario);
+$htmlRosario=str_replace(' scope="row"','',$htmlRosario);
+$htmlRosario=str_replace(' class="pna-port-left"','',$htmlRosario);
+$htmlRosario=str_replace(' class="warning"','',$htmlRosario);
+$htmlRosario=str_replace('</th>','',$htmlRosario);
+$htmlRosario=str_replace('</td>','',$htmlRosario);
+$htmlRosario=str_replace('<th>','<td>',$htmlRosario);
+$htmlRosario=explode('<td>', $htmlRosario);
+$altura=$htmlRosario[4];
+$altura=str_replace(",",".",$altura);
+$estado_tmp=$htmlRosario[7];
+$estado='-';
+$estado_tmp=trim($estado_tmp);
+switch ($estado_tmp){
+	case 'ESTAC.':
+		$estado='estacionario';
+		break;
+	case 'CRECE':
+		$estado='crece';
+		break;
+	case 'CRECE':
+		$estado='BAJA';
+		break;		
+		default:
+		$estado='';
+		break;
+}
+$respuesta="El río está en ".$altura." metros, ".$estado;
+// Fin altura Rosario
+
 
 $method = $_SERVER['REQUEST_METHOD'];
-
 // Process only when method is POST
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
@@ -15,7 +54,8 @@ if($method == 'POST'){
 	$response = new \stdClass();
 	//$response->speech = $speech;
 	//$response->displayText = $speech;
-	$response->fulfillmentText = $speech;
+	//$response->fulfillmentText = $speech;
+	$response->fulfillmentText = $respuesta;
 	$response->source = "webhook";
 	echo json_encode($response);
 }
