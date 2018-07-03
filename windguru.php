6f6bf753e5a67ac115669fab7c000f77ee0ba724
mauri82 <mauriciogz@gmail.com>
2018-07-03 19:00:16 -0300

    Update windguru.php

--- windguru.php
+++ windguru.php
@@ -1,37 +1,29 @@
 <?php
 $salida='{
-    "fulfillmentText": "Windguru Rosario",
-    "fulfillmentMessages": [
-      {
-        "card": {
-          "title": "Windguru",
-          "subtitle": "Rosario",
-          "imageUri": "http://old.windguru.cz/images/windguru_final2-huri.png",
-          "buttons": [
-            {
-              "text": "Ver",
-              "postback": "http://old.windguru.cz/int/mview.php?s=643563&m=3"
-            }
-          ]
-        }
+  "fulfillmentMessages": [
+    {
+      "text": {
+        "text": [
+          "Wind Gurú Rosario"
+        ]
       }
-    ],
-    "source": "windguru.cz",
-    "payload": {
-      "google": {
-        "expectUserResponse": true,
-        "richResponse": {
-          "items": [
-            {
-              "simpleResponse": {
-                "textToSpeech": "Windguru Rosario"
-              }
-            }
-          ]
-        }
+    },
+    {
+      "card": {
+        "title": "Windguru",
+        "subtitle": "Rosario",
+        "imageUri": "http://old.windguru.cz/images/windguru_final2-huri.png",
+        "buttons": [
+          {
+            "text": "Ver",
+            "postback": "http://old.windguru.cz/int/mview.php?s=643563&m=3"
+          }
+        ]
       }
     }
-  }';  
+  ],
+  "outputContexts": []
+}';  
 echo $salida;
 ?>
 
