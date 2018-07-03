12604401e0e789a04a4ea8608edb3f3bfcac20f5
mauri82 <mauriciogz@gmail.com>
2018-07-02 22:32:16 -0300

    Add files via upload

--- windguru.php
+++ windguru.php
@@ -1,5 +1,5 @@
 <?php
-$salida='{
+/*$salida='{
     "fulfillmentText": "Windguru Rosario",
     "fulfillmentMessages": [
       {
@@ -17,5 +17,40 @@ $salida='{
       }
     ]
   }';
+*/
+$salida='{
+    "fulfillmentText": "Windguru Rosario",
+    "fulfillmentMessages": [
+      {
+        "card": {
+          "title": "Windguru",
+          "subtitle": "Rosario",
+          "imageUri": "http://old.windguru.cz/images/windguru_final2-huri.png",
+          "buttons": [
+            {
+              "text": "Ver",
+              "postback": "http://old.windguru.cz/int/mview.php?s=643563&m=3"
+            }
+          ]
+        }
+      }
+    ],
+    "source": "windguru.cz",
+    "payload": {
+      "google": {
+        "expectUserResponse": true,
+        "richResponse": {
+          "items": [
+            {
+              "simpleResponse": {
+                "textToSpeech": "Windguru Rosario"
+              }
+            }
+          ]
+        }
+      }
+    }
+  }';  
 print $salida;
 ?>
+
